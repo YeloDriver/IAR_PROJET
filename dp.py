6e3783741511eb951b8ba97d96338ff97b561bf5
jzhao1998 <46528670+jzhao1998@users.noreply.github.com>
2021-10-29 17:19:55 +0200

    Update dp.py

--- dp.py
+++ dp.py
@@ -2,7 +2,11 @@ import robot
 import simulator
 import numpy as np
 from random import randint
-
+from matplotlib import pyplot as plt 
+def show(reward):
+    plt.plot(reward)
+    plt.title("reward of each episode")
+    plt.show()
 class dp:
     def __init__(self,map,robot):
         self.x=map.x
@@ -12,7 +16,7 @@ class dp:
         self.policy=[]
         self.epsilon = 0.01  # The threshold used to stop the interations
         self.discounted_factor = 0.99
-    
+        self.reward=[]
     def getAllState(self):
         for action in range(4):
             for battery in range(self.batteryMax):
@@ -34,16 +38,17 @@ class dp:
         self.getAllState()
         value = [100.0 for _ in range(len(self.states))]
         value_prime = [-100.0 for _ in range(len(self.states))]
-        #之后改成norme infini
-        for i in range(1000):
+        #while((q-q1).any>self.epsilon):
+        for i in range(100):
+            r=0
             policy=[]
-            value_prime=value
+            value_prime=value.copy()
             for state_ind,state in enumerate(self.states):
                 q_values=[0 for _ in range(4)]
                 for action in range(4):
                     reward, probability, newState = simu.simulate(state, action,map)
+                    r+=reward
                     q_values[action] = reward
-
                     if newState and (probability > 0):
                         newStateInd=self.states.index(newState)
                         if newStateInd is None: #有可能会不存在所有状态中？
@@ -51,7 +56,9 @@ class dp:
                         q_values[action] += self.discounted_factor * probability * value_prime[newStateInd]
                 best_action, value[state_ind] = self.best_perf(q_values)
                 policy.append(best_action)
+            self.reward.append(r)
         self.policy=policy
+        show(self.reward)
 
 if __name__ == "__main__":  # simulator写好后理论上直接跑这个可以进行测试
 
